340e1dd4511225714339aa5dc478bdb9718e01d5
QuackyMan <40685888+QuackyMan@users.noreply.github.com>
2018-06-29 09:09:41 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  client.user.setGame('w!setup for setup and w!invite for invite')
+});
+
+client.on('message', msg => {
+  if (msg.content === 'w!setup') {
+    msg.reply('You need to do 2 things to set up this bot! First you want to make 2 channels named welcome and good-bye. And then make a role named Regular.');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'w!invite') {
+    msg.reply('https://tiny.cc/welcomerbot');
+  }
+});
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+     if (!channel) return;
+     const embed = new Discord.RichEmbed()
+     .setColor('RANDOM')
+     .setThumbnail(memberavatar)
+     .addField('\:bust_in_silhouette: / User: ', `${member}`)
+     .addField('\:thumbsup: / Welcome here', `${member}`)
+     .addField('\:point_right: / Here you go. A role to you', `Regular`)
+     .setFooter(`Message sent by Welcomer`)
+     .setTimestamp()
+  
+     channel.sendEmbed(embed);
+  
+  
+    var role = member.guild.roles.find('name', 'Regular');
+  
+    member.addRole(role)
+  });
+
+  client.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'good-bye');
+    let memberavatar = member.user.avatarURL
+     if (!channel) return;
+     const embed = new Discord.RichEmbed()
+     .setColor('RANDOM')
+     .setThumbnail(memberavatar)
+     .addField('\:bust_in_silhouette: / User: ', `${member}`)
+     .addField('\:thumbsup: / Goodbye!', `${member}`)
+     .addField('\:point_right: / Now we are removing his role', `lol`)
+     .setFooter(`Message sent by Welcomer`)
+     .setTimestamp()
+  
+     channel.sendEmbed(embed);
+  
+  
+    var role = member.guild.roles.find('name', 'Regular');
+  
+    member.addRole(role)
+  });
+
+client.on('ready', () => {
+    console.log('Tjena Derpy! Boten är klar nu!')
+
+});
+
+client.login('process.env.BOT_TOKEN');
